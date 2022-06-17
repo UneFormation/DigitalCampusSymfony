8cfe946a495b5502dfa4275419dbcb0c6211589f
David Patiashvili <stratagem.david@gmail.com>
2022-06-17 10:30:09 +0200

    Create form to edit profile data

--- src/Controller/MeController.php
+++ src/Controller/MeController.php
@@ -3,7 +3,10 @@
 namespace App\Controller;
 
 use App\Controller\Trait\RoleTrait;
+use App\Form\MeFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,12 +16,22 @@ class MeController extends AbstractController
     use RoleTrait;
 
     #[Route('/', name: 'main')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
         if ($response = $this->checkRole('ROLE_USER')) {
             return $response;
         }
+        $user = $this->getUser();
+        $form = $this->createForm(MeFormType::class, $user);
+        $form->handleRequest($request);
 
-        return $this->render('me/index.html.twig', []);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($user);
+            $entityManager->flush();
+        }
+
+        return $this->render('me/index.html.twig', [
+            'userForm' => $form->createView()
+        ]);
     }
 }
---
+++ src/Form/MeFormType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MeFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstname', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control mb-3'
+                ],
+                'label' => 'Votre prénom'
+            ])
+            ->add('lastname', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control mb-3'
+                ],
+                'label' => 'Votre nom'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
